a02a1dbe3d9ef25f444701d95fa4ce25c3b30f98
gmetaxas <gmetaxas@globoplc.com>
2014-11-11 23:27:13 +0200

    Sample java app implemented

--- src/gr/gmetal/jnicrashhandlerapp/MainActivity.java
+++ src/gr/gmetal/jnicrashhandlerapp/MainActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button safeFuncBtn = (Button) findViewById(R.id.btn_safe_func);
+        Button unsafeFuncBtn = (Button) findViewById(R.id.btn_unsafe_func);
+        safeFuncBtn.setOnClickListener(this);
+        unsafeFuncBtn.setOnClickListener(this);
     }
 
 
@@ -33,4 +41,18 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.btn_safe_func) {
+			safeFunc();
+		}
+		else if (v.getId() == R.id.btn_unsafe_func) {
+			unsafeFunc();
+		}
+	}
+	
+	private native void safeFunc();
+	private native void unsafeFunc();
 }
